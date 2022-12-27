bfd7cda26ea40e0bd74a42f661cebaee237a3454
MiracleCat <miracle_1212@naver.com>
2022-12-27 16:03:32 +0900

    Initial commit

---
+++ src/main/java/mc/example/SpringbootMavenTestApplication.java
@@ -0,0 +1,13 @@
+package mc.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMavenTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMavenTestApplication.class, args);
+    }
+
+}
