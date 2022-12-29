476ac670f835d6264b243c63d30fb8f51e9ce17a
MiracleCat <miracle_1212@naver.com>
2022-12-29 15:00:03 +0900

    refactor. change compile version 17 to 11

--- src/main/java/mc/example/SpringbootMavenTestApplication.java
+++ src/main/java/mc/example/SpringbootMavenTestApplication.java
@@ -3,6 +3,9 @@ package mc.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 @SpringBootApplication
 public class SpringbootMavenTestApplication {
 
